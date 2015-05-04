51a8326bd0f8cb07ac324738072ed096e49e284c
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-05 00:53:33 +0200

    Update make.js

--- make.js
+++ make.js
@@ -2,4 +2,4 @@
 var fs = require('fs');
 var builtinModules = require('./');
 
-fs.writeFileSync('builtin-modules.json', JSON.stringify(builtinModules, null, '\t'));
+fs.writeFileSync('builtin-modules.json', JSON.stringify(builtinModules, null, '\t') + '\n');
