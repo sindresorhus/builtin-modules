32ab9f2fe30c1cad2b449d7e6058a1b4018d9b88
Sindre Sorhus <sindresorhus@gmail.com>
2016-02-18 18:06:22 +0700

    update for latest AVA

--- test.js
+++ test.js
@@ -4,11 +4,7 @@ import m2 from './static';
 
 test(t => {
 	console.log('Builtin modules:', m);
-
-	t.doesNotThrow(() => {
-		m.forEach(x => require(x));
-	});
-
+	t.notThrows(() => m.forEach(x => require(x)));
 	t.true(m.indexOf('fs') !== -1);
 	t.true(Array.isArray(m2));
 });
