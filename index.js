703d4496d55163cbe9dd96d24d17a89e13c7a9ba
Anna Henningsen <anna@addaleax.net>
2018-01-27 22:30:22 +0100

    Avoid relying on Node’s internals
    `process.binding()` is not a public API, and should
    not be used.
    
    Luckily, Node recently introduced an API that does
    exactly what builtin-modules needs:
    https://nodejs.org/api/modules.html#modules_module_builtinmodules
    
    So use that instead and keep the old path as a fallback.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const builtinModules = require('module').builtinModules;
+
 const blacklist = [
 	'freelist',
 	'sys'
 ];
 
-module.exports = Object.keys(process.binding('natives'))
+module.exports = (builtinModules || Object.keys(process.binding('natives')))
 	.filter(x => !/^_|^internal|\//.test(x) && blacklist.indexOf(x) === -1)
 	.sort();
