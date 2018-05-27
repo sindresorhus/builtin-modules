0fb4ac8abe9117193a23d68479f0002c35bf1018
Anna Henningsen <github@addaleax.net>
2018-05-27 09:14:15 +0200

    Avoid relying on Node’s internals (#5)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
+const builtinModules = require('module').builtinModules;
 
 const blacklist = [
 	'freelist',
 	'sys'
 ];
 
-module.exports = Object.keys(process.binding('natives'))
-	.filter(x => !/^_|^internal|\//.test(x) && blacklist.indexOf(x) === -1)
+module.exports = (builtinModules || Object.keys(process.binding('natives')))
+	.filter(x => !/^_|^(internal|v8|node-inspect)\/|\//.test(x) && blacklist.indexOf(x) === -1)
 	.sort();
