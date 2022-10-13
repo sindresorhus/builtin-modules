1aef3b5b1c49f27538e734441d98575c521a7f60
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2022-10-13 01:11:05 -0300

    Improve tests

--- test.js
+++ test.js
@@ -2,8 +2,13 @@ import test from 'ava';
 import builtinModulesStatic from './static';
 import builtinModules from '.';
 
-test('main', t => {
-	console.log('Builtin modules:', builtinModules);
+const nodejsMajorVersion = Number.parseInt(
+	process.version.split('.')[0].replace('v', ''),
+	10
+);
+
+test('builtinModules', t => {
+	console.log(`Builtin modules (Node.js ${nodejsMajorVersion}):`, builtinModules);
 
 	t.notThrows(() => {
 		for (const x of builtinModules) {
@@ -12,5 +17,21 @@ test('main', t => {
 	});
 
 	t.true(builtinModules.includes('fs'));
+
+	if (nodejsMajorVersion >= 14) {
+		t.true(builtinModules.includes('fs/promises'));
+	}
+
+	if (nodejsMajorVersion >= 16) {
+		t.true(builtinModules.includes('assert/strict'));
+	}
+
+	// Some Node.js versions (e.g v10) include some `v8/` and `node-inspect/` modules in `require('module').builtinModules`, but we want to ignore them
+	t.false(builtinModules.some(x => x.startsWith('v8/')));
+	t.false(builtinModules.some(x => x.startsWith('node-inspect/')));
+});
+
+test('builtinModulesStatic', t => {
 	t.true(Array.isArray(builtinModulesStatic));
+	t.true(builtinModulesStatic.every(x => typeof x === 'string'));
 });
