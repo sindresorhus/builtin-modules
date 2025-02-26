215e5e111c04b4fa6c3ef37031db30f59a68ae92
fisker <lionkay@gmail.com>
2025-02-26 21:10:59 +0800

    Fix test

--- test.js
+++ test.js
@@ -4,20 +4,26 @@ import builtinModules from './index.js';
 test('main', async t => {
 	console.log('Builtin modules:', builtinModules);
 
-	await t.notThrowsAsync((async () => {
-		for (const builtinModule of builtinModules) {
-			// Only available in latest Node.js.
-			if (builtinModule === 'trace_events' || builtinModule === 'inspector' || builtinModule === 'inspector/promises') {
-				continue;
+	for (const builtinModule of builtinModules) {
+		await t.notThrowsAsync((async () => {
+			// Only available in latest Node.js or requires flags.
+			if (
+				builtinModule === 'trace_events'
+        || builtinModule === 'inspector'
+        || builtinModule === 'inspector/promises'
+        || builtinModule === 'node:quic'
+        || builtinModule === 'node:trace_events'
+			) {
+				return;
 			}
 
 			await import(builtinModule);
-		}
-	})());
+		})(), `Can't import '${builtinModule}'.`);
+	}
 
 	t.true(Array.isArray(builtinModules));
-	t.true(builtinModules.includes('fs'));
 	t.true(builtinModules.includes('node:fs'));
+	t.true(builtinModules.includes('fs'));
 	t.true(builtinModules.includes('node:test'));
 	t.false(builtinModules.includes('test'));
 });
